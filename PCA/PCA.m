c2b6f555b73994d3f7b4d4beb0805a70d412fd35
Harini-krishnapriya <119836846+Harini-krishnapriya@users.noreply.github.com>
2023-04-30 12:24:18 +0530

    Update PCA.m

--- PCA/PCA.m
+++ PCA/PCA.m
@@ -1,13 +1,12 @@
 % Principal component analysis
 data_points=randn(70,2);
 data_points=data_points';
-data_mean=[ mean(data_points(1,:)); mean(data_points(2,:))];
+data_mean=[mean(data_points(1,:)); mean(data_points(2,:))];
 uncorrelated_data=data_points-data_mean;
-
 covariance=[0 0; 0 0];
 for j=1:10
         covariance=covariance+(uncorrelated_data(:,j))*(uncorrelated_data(:,j))';
- end
+end
 covariance=covariance/length(data_points)-1;
 [eigen_vector eigen_values]= eig(covariance);
 c=scatter(uncorrelated_data(1,:),uncorrelated_data(2,:),'filled');hold on
@@ -18,4 +17,3 @@ y2=eigen_vector(1,2)/eigen_vector(1,1)*x2;
 a=plot(x1,y1); hold on;
 b=plot(x2,y2);hold off
 legend([a,b,c],'Principal Component1','Principal Component2', 'Datapoints');
-
